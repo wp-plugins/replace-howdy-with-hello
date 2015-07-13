6d55df3357480e5fafb3c9a48e7c43c489e122a1
swadeshswain <swadeshswain@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-13 10:41:55 +0000

    New update with option page
    git-svn-id: https://plugins.svn.wordpress.org/replace-howdy-with-hello/trunk@1197833 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- readme.txt
+++ readme.txt
@@ -1,16 +1,16 @@
 === Remove Howdy ===
 Contributors: swadeshswain
-Tags: howdy, replace howdy, customize howdy, change howdy
+Tags: howdy, replace howdy, customize howdy, change howdy , custom text
 Requires at least: 3.0
 Tested up to: 4.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-Replace the "Howdy" text in the top right corner of your dashboard. 
+This plugin will Replace the "Howdy" with your custom text in the top right corner of your dashboard. 
 
 == Description ==
 
-This plugin will Replace the "Howdy" text in the top right corner of your dashboard. 
+This plugin will Replace the "Howdy" with your custom text in the top right corner of your dashboard. 
 
 
 == Screenshots ==
@@ -18,6 +18,8 @@ This plugin will Replace the "Howdy" text in the top right corner of your dashbo
 1. After
 
 2. Befor
+
+3. Option page
 
 == Installation ==
 
@@ -30,3 +32,5 @@ This plugin will Replace the "Howdy" text in the top right corner of your dashbo
 
 Version 1.0
 Plugin Replace "Howdy" in the dashboard
+Version 1.1
+Plugin Replace "Howdy" with you custom text.
--- replace-howdy.php
+++ replace-howdy.php
@@ -2,21 +2,109 @@
    /*
    Plugin Name: Replace Howdy
    Plugin URI: http://www.odrasoft.com/plugins/remove-howdy
-   Description: This plugin will replace "Howdy" in the top right corner of your Wordpress dashboard.
-   Version: 1.0
+   Description: This plugin will replace "Howdy" with your custome text in the top right corner of your Wordpress dashboard.
+   Version: 1.1
    Author: Swadesh Ranjan Swain
    Author URI: http://www.odrasoft.com
    License: GPL2
    */
+   
+   if (is_admin()) {
+   
+add_action('admin_menu', 'wp_custom_howdy');
+
+function wp_custom_howdy() {
+    add_options_page('Remove Howdy', 'Remove Howdy', 'manage_options',  basename(__FILE__), 'wp_remove_howdy_page');
+}
+function wp_remove_howdy_page() {
+?>
+<div class="wrap">
+			<h3>Replace the "Howdy" Option</h3>
+            
+            <?php
+
+	
+    if ( isset($_POST['submit']) ) { 
+        $nonce = $_REQUEST['_wpnonce'];
+        if (! wp_verify_nonce($nonce, 'php-howdy-updatesettings' ) ) {
+            die('security error');
+        }
+        $removehowdy = $_POST['removehowdy'];
+		$removehowdytext = $_POST['removehowdytext'];
+        
+   
+        update_option( 'od_removehowdy', $removehowdy );
+		 update_option( 'od_removehowdytext', $removehowdytext );
+    
+    } 
+    $od_removehowdy = get_option( 'od_removehowdy' );
+	$od_removehowdytext = get_option( 'od_removehowdytext' );
+
+	?>
+ 
+    
+			<form method="post" action="" id="php_config_page">
+				<?php wp_nonce_field('php-howdy-updatesettings'); ?>
+              
+                 
+				<table class="form-table">
+					<tbody>
+						
+                    <tr>
+						<th><label>Allow to Remove Howdy Text </label></th>
+					
+						<td>
+                                         <Input type = 'Radio' Name ='removehowdy' value= 'yes'
+ <?php if ($od_removehowdy == 'yes') echo 'checked="checked"'; ?>>
+Yes
+
+<Input type = 'Radio' Name ='removehowdy' value= 'no'
+ <?php if ($od_removehowdy == 'no') echo 'checked="checked"'; ?>>
+No
+                        </td>
+                      
+                    </tr>
+                    
+                    	
+                    <tr>
+						<th><label>Add Text On The Place of Howdy</label></th>
+					
+						<td>
+                                         <Input type = 'text' Name ='removehowdytext' value= '<?php echo $od_removehowdytext ; ?>' />
+ 
+
+                        </td>
+                      
+                    </tr>
+                    
+                     
+                                       
+                    
+                     
+                    
+                   
+					</tbody>
+				</table>
+				<p class="submit"><input type="submit" value="Save Changes" class="button-primary" id="submit" name="submit" /></p>  
+			</form>
+		</div>
+<?php
+} // get_option('od_phpcontent');
+} 
+
+
+
 
 function replace_howdy( $wp_admin_top_bar ) {
     $wp_my_account=$wp_admin_top_bar->get_node('my-account');
-    $replacetitle = str_replace( 'Howdy', 'Hello', $wp_my_account->title );
+	$howdytext = get_option('od_removehowdytext') ;
+    $replacetitle = str_replace( 'Howdy', $howdytext, $wp_my_account->title );
     $wp_admin_top_bar->add_node( array(
         'id' => 'my-account',
         'title' => $replacetitle,
     ) );
 }
-add_filter( 'admin_bar_menu', 'replace_howdy',25 );
-
 ?>
+<?php if ( get_option('od_removehowdy') == 'yes') {?>
+<?php  add_filter( 'admin_bar_menu', 'replace_howdy',25 );?>
+<?php } ?> 
